4023387779fb787983fffbe693c0c007a7a3d967
sunqiang <sun1993hlj@hotmail.com>
2016-12-05 15:23:05 +0800

    add nutrist to index from mac

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 /**
  * Created by sunqiang on 2016/11/16.
  */
+var tops
+window.onload=function () {
+    tops=0
+}
+window.onscroll=function(){
+    var t =document.documentElement.scrollTop||document.body.scrollTop;
+    console.log(t+":"+tops)
+
+
+}
