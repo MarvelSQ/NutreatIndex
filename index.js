0d8b582f744a14967d970ff85dc093a17a85cc7c
sunqiang <sun1993hlj@hotmail.com>
2016-12-12 16:45:46 +0800

    optimize performence in wechat from mac

--- index.js
+++ index.js
@@ -2,12 +2,44 @@
  * Created by sunqiang on 2016/11/16.
  */
 var tops
+var user_android=document.getElementById('user-aload')
+var user_ios=document.getElementById('user-iload')
+var nutrist_android=document.getElementById('nutrist-aload')
+var nutrist_ios=document.getElementById('nutrist-iload')
+
 window.onload=function () {
     tops=0
+    if(isWeiXin()){
+        user_android.addEventListener('click',function () {
+            jumptoUser()
+        })
+        user_ios.addEventListener('click',function () {
+            jumptoUser()
+        })
+        nutrist_android.addEventListener('click',function () {
+            jumptoNutrist()
+        })
+        nutrist_ios.addEventListener('click',function () {
+            jumptoNutrist()
+        })
+    }
 }
 window.onscroll=function(){
-    var t =document.documentElement.scrollTop||document.body.scrollTop;
-    console.log(t+":"+tops)
+    // var t =document.documentElement.scrollTop||document.body.scrollTop;
+    // console.log(t+":"+tops)
+}
+function isWeiXin(){
+    var ua = window.navigator.userAgent.toLowerCase();
 
-
+    if(ua.match(/MicroMessenger/i) == 'micromessenger'){
+        return true;
+    }else{
+        return false;
+    }
+}
+function jumptoNutrist() {
+    window.location.href="http://healthit.cn/nutreat/Home/index/consultant_app"
+}
+function jumptoUser() {
+    window.location.href="http://nutreat.cn/nutreat/index.php/Home/Index/down_page"
 }
